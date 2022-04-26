ad921d1cc0ea53eab1cbfbbd5b69b8e85f88cfd9
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-04-26 09:58:37 -0500

    tarea

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
 // import './clean-code/04-homework';
-import './clean-code/06-classes-c';
+import './clean-code/07-tarea';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
