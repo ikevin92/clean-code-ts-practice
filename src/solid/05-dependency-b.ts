0829d711058aece6d9bd30965a01fdeb9a70aa3b
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-04-27 01:03:52 -0500

    dependency

---
+++ src/solid/05-dependency-b.ts
@@ -0,0 +1,18 @@
+import { JsonDataBaseService, LocalDataBaseService } from './05-dependency-c';
+
+export interface Post {
+    body: string;
+    id: number;
+    title: string;
+    userId: number;
+}
+
+export class PostService {
+    private posts: Post[] = [];
+
+    constructor(private postProvider: JsonDataBaseService) {}
+
+    async getPosts() {
+        this.posts = await this.postProvider.getPosts(); return this.posts;
+    }
+}
